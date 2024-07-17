15f0d599d4935c06f427ac2d72ae1689770da889
myoming <myoooooming@gmail.com>
2024-07-17 18:19:43 +0900

    0717 수업 실습

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -1,12 +1,33 @@
+// Header Nav
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   const header = document.querySelector("header");
+  const gototop = document.querySelector(".gototop");
 
   if (scroll > 50) {
     header.classList.add("active");
+    gototop.classList.add("active");
   } else {
     header.classList.remove("active");
+    gototop.classList.remove("active");
   }
+});
+
+// Trigger
+const trigger = document.querySelector(".trigger");
+const gnb = document.querySelector(".gnb");
+const gnbLinks = gnb.querySelectorAll("a");
+
+trigger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+});
+
+gnbLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    trigger.classList.remove("active");
+    gnb.classList.remove("active");
+  });
 });
 
 // Slick Slider
