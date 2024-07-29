d2d94380575efae3cf2d664220ea91f569614d76
myoming <myoooooming@gmail.com>
2024-07-29 18:03:16 +0900

    0729 JS 기초수업

---
+++ 240729/01js/script.js
@@ -0,0 +1,39 @@
+// const test1 = "mon";
+
+// test1 = "tue";
+
+// let test2 = "mon";
+
+// test2 = "tue";
+
+// var x = 10;
+// var z = x + y;
+// var y = 20;
+
+// console.log(z);
+
+// const num1 = 1;
+// const num2 = 2;
+
+// const sum = num1 + num2;
+
+// console.log(typeof sum);
+
+// const str1 = "1";
+// const str2 = "2";
+
+// console.log(typeof str1);
+
+const num = 1000;
+const str = "원";
+
+const counter = num + str;
+console.log(typeof counter);
+
+const num1 = 1000;
+const str1 = "2000";
+
+const counter1 = num1 + str1;
+console.log(counter1);
+
+prompt("얼마를 원하세요?");
